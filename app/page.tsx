274a26dd9f53b8fb796f874c2485593ea4ffee23
kohsnmba25 <kohei.sn711@gmail.com>
2026-05-07 17:49:22 +0900

    update-login-ui-for-coach 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from 'next/navigation'
+
 export default function Home() {
-  return (
-    <main className="flex flex-1 items-center justify-center p-8">
-      <h1 className="text-2xl font-semibold">野球ノート</h1>
-    </main>
-  );
+  redirect('/login')
 }
